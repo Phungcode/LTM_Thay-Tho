b535844211705b0b14a48d847a3acefde3c9e4f1
Phungcode <Tkakapanter007@gmail.com>
2022-04-04 19:25:27 +0700

    new

---
+++ Bài thực hành số (Tuần 6)/Client/src/Tuan6/Client_Tuan6.java
@@ -0,0 +1,53 @@
+package Tuan6;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.net.Socket;
+import java.util.Scanner;
+
+public class Client_Tuan6 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Socket SK;
+		try {
+			SK = new Socket ("localhost",2001);
+			System.out.print("Đã Kết Nối Thành Công \n");
+			
+			InputStream in = SK.getInputStream();
+			InputStreamReader inRead = new InputStreamReader(in);
+			BufferedReader buffRead = new BufferedReader(inRead);
+			
+			OutputStream osToClient = SK.getOutputStream();
+			OutputStreamWriter write2client = new OutputStreamWriter(osToClient);
+			BufferedWriter buffWrite = new BufferedWriter(write2client);
+			
+			Scanner  banphim = new Scanner(System.in);
+		
+			while(true) {
+				System.out.print("\n Client: ");
+				String guitra = banphim.nextLine();
+				
+				buffWrite.write(guitra +"\n");
+				buffWrite.flush();
+				
+				String nhanchuoi = buffRead.readLine();
+				System.out.print("Server" +nhanchuoi);
+				
+				if(guitra.equals("Bai")) break;
+			}
+		SK.close();
+		}
+		catch (IOException e) {
+
+			System.out.print("Đã ngắt kết nối....");
+			e.printStackTrace();
+		}
+	}
+
+}
